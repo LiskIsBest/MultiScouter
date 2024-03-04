091926db4d38dfb4c09cc24883f3233d9892c027
Darien Moore <liskisbest@proton.me>
2024-03-04 09:59:55 -0600

    mastery score site works

--- mastery_scrape.py
+++ mastery_scrape.py
@@ -1,1 +1,10 @@
 # https://championmastery.gg/
+# https://championmastery.gg/player?riotId=lisk%23lisk&region=NA&lang=en_US
+from urllib.parse import quote
+import httpx
+from bs4 import BeautifulSoup
+
+username = input("Enter username and tag (ex: Lisk#Lisk): ")
+
+base_url = f"https://championmastery.gg/player?riotId={quote(username)}&region=NA&lang=en_US"
+
