be36b3107703ddf13c1893fa5f9be24d0bfbf05f
Darien Moore <LiskIsBest@gmail.com>
2024-03-17 01:08:10 -0500

    created directory for webapp

---
+++ webapp/app.py
@@ -0,0 +1,11 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def index():
+    return {"message":"Hello World"}
