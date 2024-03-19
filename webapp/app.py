e213e18ea88359c60e3a3236e3bb626b8ad130bd
Darien Moore <liskisbest@proton.me>
2024-03-19 15:23:59 -0500

    redoing webapp idea

--- webapp/app.py
+++ webapp/app.py
@@ -1,74 +1,17 @@
 import os
 
-import httpx
 from dotenv import load_dotenv
-
-from starlette.config import Config
-from starlette.middleware.sessions import SessionMiddleware
-from authlib.integrations.starlette_client import OAuth, OAuthError
-
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse
-
-load_dotenv()
 
 app = FastAPI()
-
-GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID') or None
-GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET') or None
-if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
-    raise BaseException('Missing env variables')
-
-SECRET_KEY = os.getenv('SECRET_KEY') or None
-if SECRET_KEY is None:
-    raise 'Missing SECRET_KEY'
-
-config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
-starlette_config = Config(environ=config_data)
-oauth = OAuth(starlette_config)
-oauth.register(
-    name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile https://www.googleapis.com/auth/spreadsheets'},
-)
-app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.route('/auth')
-async def auth(request: Request):
-    # try:
-    #     access_token = await oauth.google.authorize_access_token(request)
-    # except OAuthError:
-    #     print("failed redirecting")
-    #     return RedirectResponse(url='/')
-    access_token = await oauth.google.authorize_access_token(request)
-    print(access_token)
-    user_data = await oauth.google.parse_id_token(request, access_token)
-    request.session['user'] = dict(user_data)
-    return RedirectResponse(url='/')
-
-@app.route('/login')
-async def login(request: Request):
-    redirect_uri = request.url_for('auth')
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-@app.route('/logout')
-async def logout(request: Request):
-    request.session.pop('user', None)
-    return RedirectResponse(url='/')
-
-
-@app.route("/", methods=['GET'])
+@app.get("/")
 async def index(request: Request):
-    # return templates.TemplateResponse(
-    #     request=request, name="index.html"
-    # )
-    user = request.session.get('user')
-    if user:
-        name = user.get('name')
-        return HTMLResponse(f'<p>Hello {name}!</p><a href=/logout>Logout</a>')
-    return HTMLResponse('<a href=/login>Login</a>')
+    return templates.TemplateResponse(request=request, name="index.html")
+    
