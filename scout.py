b10008e3e82fba593044f5a60a19f0f174933381
Darien Moore <liskisbest@proton.me>
2024-03-20 14:35:11 -0500

    move player stat gathering to multiprocess

--- scout.py
+++ scout.py
@@ -1,20 +1,17 @@
+import multiprocessing
 import httpx
-from player_stats import PlayerData, op_to_names
+from player_stats import PlayerData, op_to_names, UserNotFoundError
 
 team_name = input("Enter the team name: ")
 multi_link = input("Enter the op.gg multi link: ")
 
-with open(f"{team_name.replace(' ', '_')}.txt", 'w', encoding='utf-8') as f:
-    f.write(f"Team: {team_name}\n")
-    f.write(f'Multi: {multi_link}\n')
-    player_list = op_to_names(multi_link)
-    player_data = []
-    for p in player_list:
-        http_client = httpx.Client()
-        print(f'starting {p}')
-        d = PlayerData(riot_id=p, http_client=http_client)
-        player_data.append({
-            'name':p,
+def make_player_data(player):
+    print(f'scouting {player}')
+    client = httpx.Client()
+    try:
+        d = PlayerData(player, http_client=client)
+        data = {
+            'name':player,
             'role':d.most_played_role,
             'rank':d.rank_stats['rank'],
             'LP':d.rank_stats['lp'],
@@ -24,8 +21,22 @@ with open(f"{team_name.replace(' ', '_')}.txt", 'w', encoding='utf-8') as f:
             'flex_champs':d.flex_champs,
             'champ_mastery':d.champion_mastery,
             'opgg_link':d.opgg_link,
-        })
-        print(f'finished {p}')
+            }
+        print(f'finished {player}')
+        return data
+    except UserNotFoundError:
+        print(f'riot_id: {player} was not found... skipping')
+        return None
+
+def gen_players(players: list[str]):
+    with multiprocessing.Pool() as pool:
+        return pool.map(make_player_data, players)
+
+with open(f"{team_name.replace(' ', '_')}.txt", 'w', encoding='utf-8') as f:
+    f.write(f"Team: {team_name}\n")
+    f.write(f'Multi: {multi_link}\n')
+    player_list = op_to_names(multi_link)
+    player_data = [p for p in gen_players(player_list) if p != None]
     for p in player_data:
         print(f'writing {p["name"]}')
         f.write('\n----\n')
